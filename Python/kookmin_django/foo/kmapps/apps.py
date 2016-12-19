0ed74e2e2bfc83ca5752ab13157f73e835467b45
korea7030 <korea7030@naver.com>
2016-12-19 11:06:25 +0900

    20161219 commit

---
+++ Python/kookmin_django/foo/kmapps/apps.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from django.apps import AppConfig
+
+
+class KmappsConfig(AppConfig):
+    name = 'kmapps'
--- Python/kookmin_django/imdae_project/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from imdae_project.models import LeaseData
-# Create your views here.
-def index(request):
-	return render(request, 'imdae_project/index.html')
